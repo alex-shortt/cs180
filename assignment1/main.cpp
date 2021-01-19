810df7b2dd04e368ad7cae2f6ce3173f816c12f8
alex-shortt <alexander.shortt@gmail.com>
2021-01-19 00:08:42 -0800

    finish assignment 1

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -22,8 +22,7 @@ int main() {
     // Write the 3×3 transformation matrix for a 45 degrees
     // clockwise rotation in 2D (assuming homogeneous coordinates) 
     // Use the << operator to create the rot_45 matrix (WRITE BELOW)
-
-
+    rot_45 << 0.7071, -0.7071, 0, 0.7071, 0.7071, 0, 0, 0, 1.0;
 
     std::cout << rot_45 << std::endl;
     std::cout << "------------------------\n";
@@ -37,7 +36,7 @@ int main() {
 
     // Write the 4×4 transform matrix to move a point by (3,4,5)
     // Use the << operator to create the translation matrix (WRITE BELOW)
-
+    translation << 1.0, 0, 0, 3.0, 0, 1.0, 0, 4.0, 0, 0, 1.0, 5.0, 0, 0, 0, 1.0;
 
 
     std::cout << translation << std::endl;
@@ -51,7 +50,20 @@ int main() {
     // Print the matrices to std out with each matrix in a new line
     // WRITE BELOW
 
+    Eigen::Matrix3f base, translate_to_origin, scale, translate_back;
+    base << 1.0, 0, 0, 0, 1.0, 0, 0, 0, 1.0;
+    translate_to_origin << 1.0, 0, -2.0, 0, 1.0, -3.0, 0, 0, 1.0;
+    scale << 1.0, 0, 0, 0, 1.0, 0, 0, 0, 1.0;
+    translate_back << 1.0, 0, 3.0, 0, 1.0, 2.0, 0, 0, 1.0;
 
+    std::cout << "Start with base translation \n";
+    std::cout << base << std::endl;
+    std::cout << "Translate to the origin \n";
+    std::cout << translate_to_origin * base << std::endl;
+    std::cout << "Scale to new size \n";
+    std::cout << scale * translate_to_origin * base << std::endl;
+    std::cout << "Translate Back \n";
+    std::cout << translate_back * scale * translate_to_origin * base << std::endl;
 
     return 0;
 }
